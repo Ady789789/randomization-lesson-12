991dc3187a512f8389e2e5aca82584ad2ea8111d
adina778899 <adinamemetea@yahoo.com>
2025-01-28 12:23:34 -0600

    Initial Commit

---
+++ randomization lesson 11/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  randomization lesson 11
+//
+//  Created by natural health on 1/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ randomization lesson 11/randomization_lesson_11App.swift
@@ -0,0 +1,17 @@
+//
+//  randomization_lesson_11App.swift
+//  randomization lesson 11
+//
+//  Created by natural health on 1/28/25.
+//
+
+import SwiftUI
+
+@main
+struct randomization_lesson_11App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
